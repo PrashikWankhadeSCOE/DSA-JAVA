10bd4bbab7f98a8e06de092c87a70f3aa02d66b9
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-09-14 21:56:54 +0530

    Create Program16.java

---
+++ Array/Array_Basic/Program16.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Program16 {
+    public static void main (String [] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the string having 0 and 1");
+        String str = br.readLine();
+        System.out.println(lastIndexofOne(str));
+    }
+    static int lastIndexofOne(String str){
+        int arr[] = new int[str.length()];
+        for(int i = 0;i<arr.length;i++){
+            arr[i]=str.charAt(i)-0;
+        }
+        int rem = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]=='1'-0){
+                rem = i;
+            }
+        }
+        return rem;
+    }
+}
