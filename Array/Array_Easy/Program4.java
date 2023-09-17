19e5cbea55d23e1462e23a76509f2d8981c8befe
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-09-17 14:35:27 +0530

    Add files via upload

---
+++ Array/Array_Easy/Program4.java
@@ -0,0 +1,35 @@
+package Array_Easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Program4 {
+    public static void main (String [] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the size of array");
+        int n = Integer.parseInt(br.readLine());
+
+        int arr[] = new int [n];
+        
+        System.out.println("Enter Elements in array");
+        for(int i=0 ;i< arr.length;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println("Enter the Sum");
+        int k = Integer.parseInt(br.readLine());
+
+        System.out.println(getPairsCount(arr,k));
+    }
+    static int getPairsCount(int[] arr, int k) {
+        int count = 0;
+        for(int i = 0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]+arr[j]==k){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
