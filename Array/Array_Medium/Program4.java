87b3fdef840ff411c8ad9be1372ccc55c46fec72
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-09-19 14:30:31 +0530

    Kth Smallest Element

---
+++ Array/Array_Medium/Program4.java
@@ -0,0 +1,39 @@
+package Array_Medium;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Program4 {
+    public static void main (String [] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the size of array");
+        int n = Integer.parseInt(br.readLine());
+
+        int arr[] = new int [n];
+        
+        System.out.println("Enter Elements in array");
+        for(int i=0 ;i< arr.length;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println("Enter k");
+        int k = Integer.parseInt(br.readLine());
+
+        System.out.println(kthSmallestNumber(arr,k));
+    }
+    static int kthSmallestNumber(int [] arr,int k){
+        int temp = 0;
+        for(int i = 0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        if(k==0)
+        return arr[0];
+        return arr[k-1];
+    }
+}
