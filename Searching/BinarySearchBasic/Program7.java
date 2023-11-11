342938f9b698697bf833e27e43ed1b1e91b0b708
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-11-11 20:23:56 +0530

    done till here

--- Searching/BinarySearchBasic/Program7.java
+++ Searching/BinarySearchBasic/Program7.java
@@ -4,3 +4,50 @@
  * For example, given the input array arr = [0, 0, 1, 1, 1, 1, 1], the function should
  * return 5 as there are five occurrences of the digit 1 in the array.
  */
+
+import java.util.*;
+
+class Demo{
+	int noofOnes(int arr[],int start,int end){
+		if(start<=end){
+			int mid= start+(end-start)/2;
+
+			if(arr[mid] ==1 && (mid==0 || arr[mid-1]==0)){
+				return (end-mid+1)+noofOnes(arr,start,mid-1);
+			}
+			if(arr[mid]==0){
+				return noofOnes(arr,mid+1,end);
+			}
+			return noofOnes(arr,start,mid-1);
+		}
+		return 0;
+	}
+	/*
+	int noofOnes(int arr[]){
+		int start = 0;
+		int end = arr.length-1;
+		while(start<end){
+			int mid= start+ (end-start)/2;
+			if(arr[mid]==1 && (mid ==0 || arr[mid-1]==0)){
+				return arr.length-mid;
+			}
+			if(arr[mid]==0){
+				start = mid+1;
+			}
+			else{
+				end = mid-1;
+			}
+		}
+		return -1;
+	}
+	*/
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0;i<n;i++){
+			arr[i] = sc.nextInt();
+		}
+		System.out.println(new Demo().noofOnes(arr,0,arr.length-1));
+	}
+}
--- Searching/BinarySearchBasic/Program8.java
+++ Searching/BinarySearchBasic/Program8.java
@@ -7,3 +7,36 @@
  * the sorted order.
  *
  */
+import java.util.*;
+class Demo{
+
+	/*
+	 * TC : O(logN)
+	 * SC : O(1)
+	 */
+	int insert(int arr[],int target,int start,int end,int x){
+		if(start<end){
+			int mid = start + (end- start)/2;
+			if(arr[mid] == target)
+				return mid;
+			if(arr[mid]>target){
+				x = mid; 
+				return insert(arr,target,start,mid,x);
+			}
+			else{
+				x = mid+1;
+				return insert(arr,target,mid+1,end,x);
+			}
+		}
+		return x;
+	}
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int target = sc.nextInt();
+		int arr[] = new int[n];
+		for(int i = 0;i<n;i++)
+			arr[i] = sc.nextInt();
+		System.out.println(new Demo().insert(arr,target,0,n-1,-1));
+	}
+}
