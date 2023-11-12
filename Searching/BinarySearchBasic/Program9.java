536f83993cb6768eecee2b92a863e29af624663d
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-11-12 22:13:33 +0530

    till here

--- Searching/BinarySearchBasic/Program9.java
+++ Searching/BinarySearchBasic/Program9.java
@@ -11,3 +11,36 @@
  * ]
  * target element 5, the function should return true, as 5 is present in the matrix.
  */
+
+import java.util.*;
+
+class Demo{
+	boolean check(int arr[][],int target){
+		int row = 0;
+		int col = arr.length-1;
+		while(row>=0 && col<arr.length){
+			if(arr[row][col] == target)
+				return true;
+			else if(arr[row][col]>target)
+				col--;
+			else
+				row++;
+		}
+		return false;
+	}
+
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int arr[][] = new int [n][n];
+		
+		for(int i = 0;i<n;i++){
+			for(int j = 0;j<n;j++){
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		int target = sc.nextInt();
+		System.out.println(new Demo().check(arr,target));
+	}
+}
