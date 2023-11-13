53d9bf6611c482f59f42bf794bd4dcc5b9cbc786
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2023-11-13 22:09:01 +0530

    Add files via upload

---
+++ Searching/BinarySearch_2/Program1.java
@@ -0,0 +1,14 @@
+/*
+ * Given a sorted array with distinct elements.
+ *
+ * Find the floor of no k in the array
+ *
+ * floor => reatest element <=k
+ * input : Arr[2,3,6,9,10,11,14,18]
+ * output :
+ * k = 5 floor = 3
+ * k = 4 floor = 3
+ * k = 6 floor = 6
+ *
+ */
+
