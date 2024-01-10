545bc8ecbd66d6da0d39e8bd4eaace68f1c4b0ad
Prashik Wankhade <111126326+PrashikWankhadeSCOE@users.noreply.github.com>
2024-01-10 19:44:46 +0530

    All Questions of que done que 4 also done

--- Queue/Program4.java
+++ Queue/Program4.java
@@ -35,4 +35,62 @@
  *     1 <= values of the stack <= 100
  */
 
+import java.util.*;
 
+class Solution{
+	Queue<Integer> q1 = new LinkedList();
+	Queue<Integer> q2 = new LinkedList();
+
+	void push(int a){
+		q1.offer(a);
+	}
+	int pop(){
+		int x = q1.size();
+		int val = -1;
+		while(x>1){
+			if(!q1.isEmpty())
+				q2.offer(q1.poll());
+			x--;
+		}
+		
+		if(!q1.isEmpty())
+			val = q1.poll();
+
+		while(!q2.isEmpty()){
+			q1.offer(q2.poll());
+		}
+		return val;
+	}
+}
+class Client{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
+		
+		Solution s = new Solution();
+		
+		System.out.println("Enter the number of queries you want to add");
+		int q = sc.nextInt();
+		
+		while(q>0){
+			int a = sc.nextInt();
+			switch(a){
+				case 1:{
+					System.out.println("Enter data you want to add in Stack");
+					s.push(sc.nextInt());
+					break;
+				}
+				case 2:{
+					int z = s.pop();
+					if(z!=-1){
+						System.out.println(z);
+					}
+					break;
+				}
+				default :{
+					break;
+				}
+			}
+			q--;
+		}
+	}
+}
